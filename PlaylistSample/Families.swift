a5632b1eeb3ddd8670ae9adacd0bc8a8ff67c703
ehh1976 <ehh.1976@gmail.com>
2015-02-09 21:25:50 -0800

    Fix dictionary issue by adding init method to the Families struct

--- PlaylistSample/Families.swift
+++ PlaylistSample/Families.swift
@@ -9,42 +9,46 @@
 import Foundation
 
 struct Families{
-    let families = [
-        [
-            "LastName": "Hillel",
-            "FirstNames":["Eric","Ayelet","Shay","Noy","Itay"],
-            "PetName": "Lucky",
-            "Icon": "Hillel.pdf"
-        ],
-        [
-            "LastName": "Green",
-            "FirstNames":["Alon","Keren","Tom"],
-            "PetName": "",
-            "Icon": "Green.pdf"
-        ],
-        [
-            "LastName": "Hans",
-            "FirstNames":["Mathiew","Dana"],
-            "PetName": "Milki",
-            "Icon": "Hans.pdf"
-        ],
-        [
-            "LastName": "Vermont",
-            "FirstNames":["Alex","Miriam","Sam","Dan"],
-            "PetName": "As",
-            "Icon": "Vermont.pdf"
-        ],
-        [
-            "LastName": "Bart",
-            "FirstNames":["Mike","Grace","Dina","William","Mark"],
-            "PetName": "",
-            "Icon": "Bart.pdf"
-        ],
-        [
-            "LastName": "Cohen",
-            "FirstNames":["Sami","Lilane"],
-            "PetName": "Dingo",
-            "Icon": "Cohen.pdf"
+    var families = [[:]]
+    
+    init(){
+        families = [
+            [
+                "LastName": "Hillel",
+                "FirstNames":["Eric","Ayelet","Shay","Noy","Itay"],
+                "PetName": "Lucky",
+                "Icon": "Hillel.pdf"
+            ],
+            [
+                "LastName": "Green",
+                "FirstNames":["Alon","Keren","Tom"],
+                "PetName": "",
+                "Icon": "Green.pdf"
+            ],
+            [
+                "LastName": "Hans",
+                "FirstNames":["Mathiew","Dana"],
+                "PetName": "Milki",
+                "Icon": "Hans.pdf"
+            ],
+            [
+                "LastName": "Vermont",
+                "FirstNames":["Alex","Miriam","Sam","Dan"],
+                "PetName": "As",
+                "Icon": "Vermont.pdf"
+            ],
+            [
+                "LastName": "Bart",
+                "FirstNames":["Mike","Grace","Dina","William","Mark"],
+                "PetName": "",
+                "Icon": "Bart.pdf"
+            ],
+            [
+                "LastName": "Cohen",
+                "FirstNames":["Sami","Lilane"],
+                "PetName": "Dingo",
+                "Icon": "Cohen.pdf"
+            ]
         ]
-    ]
+    }
 }
